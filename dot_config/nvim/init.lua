b86d0c9fe422176e1f897299905d7400f13eafa3
Erik Wright <hey@erikwright.dev>
2025-02-11 20:06:06 +0100

    add tofi

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -256,6 +256,30 @@ require("lazy").setup({
         },
       },
     },
+    {
+      "akinsho/toggleterm.nvim",
+      version = "*",
+      opts = {
+        open_mapping = [[<c-\>]],
+        direction = "float",
+        float_opts = {
+          border = "single",
+          winblend = 3,
+        },
+        winbar = {
+          enabled = false,
+          name_formatter = function(term)
+            return term.name
+          end,
+        },
+        responsiveness = {
+          -- breakpoint in terms of `vim.o.columns` at which terminals will start to stack on top of each other
+          -- instead of next to each other
+          -- default = 0 which means the feature is turned off
+          horizontal_breakpoint = 135,
+        },
+      },
+    },
   },
   install = { colorscheme = { "tokyonight-night" } },
   checker = { enabled = true },
