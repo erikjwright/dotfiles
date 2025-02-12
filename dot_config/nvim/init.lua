764b7d70976c406d25c99256dbc6dc7f1d6ed025
erikjwright <hey@erikwright.dev>
2025-02-12 18:01:21 +0100

    add wofi

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -24,7 +24,7 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   desc = "Hightlight selection on yank",
   pattern = "*",
   callback = function()
-    vim.highlight.on_yank({ higroup = "IncSearch", timeout = 180 })
+    vim.highlight.on_yank({ higroup = "IncSearch", timeout = 60 })
   end,
 })
 
